619a8341692cd76fffb3be8d0753fe742d632513
sai chand <40193113+rapchu@users.noreply.github.com>
2020-10-24 10:24:08 +0530

    Add files via upload

---
+++ components/footer.php
@@ -0,0 +1,47 @@
+<?php
+  echo'
+ 
+
+    <footer class="footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-4 col-xs-6">
+            <h5>CITIES</h5>
+            <ul class="list-unstyled">
+              <li><a href="delhi.php">DELHI</a></li>
+			  <li><a href="hyderabad.php">HYDERABAD</a></li>
+			  <li><a href="mumbai.php">MUMBAI</a</li>
+			  <li><a href="chennai.php">CHENNAI</a></li>
+            </ul>
+          </div>
+          <div class="col-sm-4 col-sm-6">
+            <h5>SMARTPARK</h5>
+            <ul class="list-unstyled">
+              <li><a href="faq.php">Support</a></li>
+              <li><a href="contact.php">Contact</a></li>
+              <li><a href="#loginModal" data-toggle="modal" data-target="#loginModal">Sign In</a></li>
+            </ul>
+          </div>
+          <div class="col-xs-12 col-sm-4">
+            <div class="nav navbar-nav" style="padding: 40px 10px;">
+              <a class="btn btn-social-icon btn-facebook" href="http://www.facebook.com"><i class="fa fa-facebook fa-2x"></i></a>
+              <a class="btn btn-social-icon btn-twitter" href="http://twitter.com/"><i class="fa fa-twitter fa-2x"></i></a>
+              <a class="btn btn-social-icon btn-youtube" href="http://youtube.com/"><i class="fa fa-youtube fa-2x"></i></a>
+              <a class="btn btn-social-icon" href="#"><i class="fa fa-envelope-o fa-2x"></i></a>
+            </div>
+          </div>
+          <div class="col-xs-12">
+            <p style="padding:20px;"></p>
+            <p align=center>Copyright © 2020 SMARTPARK</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+  <script src="https://code.jquery.com/jquery-3.3.1.min.js" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" crossorigin="anonymous"></script>
+
+  ';
+
+?>
